f9fe4cd41f1cc64ba9f709ce460dbea572e0e5f5
John Lindquist <johnlindquist@gmail.com>
2020-12-03 16:40:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,18 @@
 "Cloning the repo to ~/.js"
 git clone https://github.com/johnlindquist/.js.git ~/.js
 
-"Accessing nvm"
-. ~/.nvm/nvm.sh
+echo "Accessing nvm"
+if [ -f ~/.nvm/nvm.sh ]; then
+  echo 'sourcing nvm from ~/.nvm'
+  . ~/.nvm/nvm.sh
+elif command -v brew; then
+  # https://docs.brew.sh/Manpage#--prefix-formula
+  BREW_PREFIX=$(brew --prefix nvm)
+  if [ -f "$BREW_PREFIX/nvm.sh" ]; then
+    echo "sourcing nvm from brew ($BREW_PREFIX)"
+    . $BREW_PREFIX/nvm.sh
+  fi
+fi
 
 echo "Installing the latest version of Node.js"
 nvm install node
