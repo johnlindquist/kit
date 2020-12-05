ffa61bbad6c1459067f0a060582049bd109cf583
johnlindquist <johnlindquist@gmail.com>
2020-12-05 09:32:08 -0700

    adding a paste-note demo

---
+++ src/paste-note.mjs
@@ -0,0 +1,14 @@
+#!js
+
+import { format } from "date-fns"
+
+const date = format(new Date(), "yyyy-MM-dd-hh-mm-ss")
+const fileName = date + ".md"
+
+const template = `
+Your pasted contents are here:
+${paste()}
+`.trim()
+
+await writeFile(fileName, template)
+code(fileName)
