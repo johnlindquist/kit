50130b2616c4f42c5ea76b10b3040326dbf19458
johnlindquist <johnlindquist@gmail.com>
2020-12-05 12:57:37 -0700

    adding datamuse scripts

---
+++ src/alfred-describe.mjs
@@ -0,0 +1,29 @@
+#!js
+
+let query = $1.replace(/ /g, "+")
+
+let response = await get(
+  `https://api.datamuse.com/words?rel_jjb=${query}&md=d`
+)
+
+let items = response.data.map(
+  ({ word, score, tags, defs }) => {
+    return {
+      title: word,
+      subtitle:
+        defs
+          ?.map(def => def.replace(/.*\t/, ""))
+          .join("; ") || "",
+      arg: word,
+      variables: { word, score, tags, defs },
+    }
+  }
+)
+
+let out = {
+  variables: {
+    query,
+  },
+  items,
+}
+console.log(JSON.stringify(out))
