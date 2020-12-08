e4c5ad9a719c927f52727b55e096f8702a18a3af
johnlindquist <johnlindquist@gmail.com>
2020-12-08 12:29:24 -0700

    updating install messaging

--- config/install.sh
+++ config/install.sh
@@ -3,13 +3,13 @@
 "Cloning the repo to $JS_PATH"
 git clone https://github.com/johnlindquist/.js.git $JS_PATH
 
-echo "Installing the latest version of Node.js"
+echo "Downloading node.js to your .js directory"
 $JS_PATH/config/install-node.sh --prefix $JS_PATH/bin/.node --yes
 
 export JS_NODE=$JS_PATH/bin/.node/bin/node
 export JS_NPM=$JS_PATH/bin/.node/bin/npm
 
-echo "Attaching .js to the latest node and npm versions"
+echo "Attaching .js to the the downloaded node and npm"
 $JS_PATH/config/create-jsrc.sh
 
 echo "Linking included scripts"
@@ -32,4 +32,4 @@ echo "Creating .env file"
 $JS_PATH/config/create-env.sh
 
 echo "Verify your installation: "
-echo "type 'joke' and hit enter"
+echo "type 'js' and hit enter"
