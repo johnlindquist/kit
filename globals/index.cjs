6e79a4968b1a9840c49604a2dcb5d6bb0688804a
johnlindquist <johnlindquist@gmail.com>
2020-12-08 13:29:14 -0700

    updating template

--- globals/index.cjs
+++ globals/index.cjs
@@ -208,12 +208,37 @@ Aborting...`)
  * You can now run this script with \`${name}\` in your terminal
  */
 
+console.log(\`${env.USER} made a ${name} script!\`)
+
 /**
- * Add whatever JavaScript you want to this file
- * Install npm packages with \`js i some-npm-package\`
+ * First, let's accept an argument and log it out:
  */
 
-console.log(\`${env.USER} made a ${name} script!\`)
+// let user = await arg(0)
+
+// console.log(user)
+
+/**
+ * Second, let's query the github api with our argument
+ * Uncomment the following lines and run \`${name} ${env.USER}\` (assuming this is your github username)
+*/
+
+// let response = await get(\`https://api.github.com/users/\${user}\`)
+
+// console.log(response.data)
+
+/**
+ * Finally, let's write the data to a file 
+ * Uncomment the following lines and re-run the command
+*/
+
+// await writeFile(user + ".json", JSON.stringify(response.data))
+
+/**
+ * Congratulations! You're ready to explore the wonderful world of JavaScript Scripts.
+ * You've probably noticed the helper functions (arg, get, and writeFile). 
+ * Run \`js globals\` to explore all the helpers available.
+*/
 
 /**
  * Disable these comments in the future by running "js env" then adding the following link to the .env:
@@ -256,8 +281,8 @@ promptForArg = async message => {
   return input.name
 }
 
-arg = async (index, name) => {
-  return args[index] || (await promptForArg(name))
+arg = async (index, message = "Input: ") => {
+  return args[index] || (await promptForArg(message))
 }
 
 getScripts = async () => {
