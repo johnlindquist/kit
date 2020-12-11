c35b2c2d07ada610fef96d4b6f53cf33a55d27ea
johnlindquist <johnlindquist@gmail.com>
2020-12-10 23:17:16 -0700

    fix path

--- config/install.sh
+++ config/install.sh
@@ -34,7 +34,7 @@ complete "Created script wrappers in bin dir"
 if grep -q $SIMPLE_PATH'/.simplerc' ~/.zshrc; then
   echo "Source already added to .zshrc"
   else
-  echo -n '\nexport PATH=\$PATH:'$SIMPLE_PATH >> ~/.zshrc
+  echo -n '\nexport PATH="$PATH:'$SIMPLE_PATH'/bin" >> ~/.zshrc
   echo -n '\nsource '$SIMPLE_PATH'/.simplerc' >> ~/.zshrc
 fi
 complete "Added simple to .zshrc"
