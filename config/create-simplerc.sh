c1b7142f73bbb62b9261f964788ecc8085909649
johnlindquist <johnlindquist@gmail.com>
2020-12-10 23:01:10 -0700

    path fix

--- config/create-simplerc.sh
+++ config/create-simplerc.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env zsh
 cat > $SIMPLE_PATH/.simplerc <<EOF
 
-export SIMPLE_PATH=$SIMPLE_PATH
 export SIMPLE_NODE=$SIMPLE_NODE
 export SIMPLE_NPM=$SIMPLE_NPM
-
-path+=$SIMPLE_PATH/bin
-export PATH
+export SIMPLE_PATH=$SIMPLE_PATH
+export PATH="$PATH:$SIMPLE_PATH"
 EOF
