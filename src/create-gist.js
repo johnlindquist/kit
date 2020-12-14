dc1d223ffb0e7b5b03fd3dabfbf718e5c8fc0e84
johnlindquist <johnlindquist@gmail.com>
2020-12-14 06:53:24 -0700

    adding create-gist

---
+++ src/create-gist.js
@@ -0,0 +1,44 @@
+//Description: Create a gist from a selected file
+
+let basePath = process.env.HOME
+let { value: filePath } = await prompt({
+  message: "Select a file",
+  type: "file",
+  basePath,
+})
+
+filePath = path.join(basePath, filePath)
+let file = _.last(filePath.split(path.sep))
+
+let { value: isPublic } = await prompt({
+  message: "Should the gist be public?",
+  type: "confirm",
+})
+
+const body = {
+  files: {
+    [file]: {
+      content: await readFile(filePath, "utf8"),
+    },
+  },
+  public: isPublic,
+}
+
+let config = {
+  headers: {
+    Authorization:
+      "Bearer " +
+      (await env("GITHUB_GIST_TOKEN", {
+        message: `
+> Create a gist token: https://github.com/settings/tokens/new      
+Set env ${chalk.yellow("GITHUB_GIST_TOKEN")}:`,
+      })),
+  },
+}
+const response = await post(
+  `https://api.github.com/gists`,
+  body,
+  config
+)
+
+console.log(response.data.html_url)
