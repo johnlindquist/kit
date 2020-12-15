92075b55d9a4ab77ef0766dcd0376cb899603be3
johnlindquist <johnlindquist@gmail.com>
2020-12-15 11:53:50 -0700

    fixing profile

--- config/link-profile.sh
+++ config/link-profile.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-  echo "---------------"
-  echo "The following is your simple PROFILE:"
-  echo $SIMPLE_PROFILE
-  echo "---------------"
+
 # Forked from Yarn's install scripts
 link_profile() {
   printf "$cyan> Adding to \$PATH...$reset\n"
   SOURCE_STR="\nsource $SIMPLE_PATH/.simplerc\n"
-
-  echo "---------------"
-  echo $SIMPLE_PROFILE
-  echo "---------------"
 
   if [ -z "${SIMPLE_PROFILE-}" ] ; then
     printf "$red> Profile not found. Tried ${SIMPLE_PROFILE} (as defined in \$PROFILE), ~/.bashrc, ~/.bash_profile, ~/.zshrc, and ~/.profile.\n"
