6fdd6bedb62164de8af2584fb352ce73cfeed1d8
johnlindquist <johnlindquist@gmail.com>
2020-12-17 15:52:12 -0700

    fail plz

--- templates/test.js
+++ templates/test.js
@@ -1,6 +1,8 @@
 let response = await get(
   `https://api.github.com/repos/johnlindquist/simplescripts`
 )
-echo(response.data.name + " is working!")
+echo(response.data.name + " is working! LIES!!!")
 
 spawn(`exit`, ["1"], { stdio: "inherit" })
+
+echo("HOW IS THIS NOT EXITING!!!!!")
