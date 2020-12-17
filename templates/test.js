dff1c3928932e5bd7eb5efb68b2a5f6e8f6a43e6
John Lindquist <johnlindquist@gmail.com>
2020-12-17 14:17:42 -0700

    Update test.js

--- templates/test.js
+++ templates/test.js
@@ -1,4 +1,8 @@
 let response = await get(
   `https://api.github.com/repos/johnlindquist/simplescripts`
 )
-echo(response.data.name)
+echo(response.data.name + " is working!")
+
+if (response.data.name != "testing failure") {
+  exit()
+}
