bc466c1c01044cc6d1d2c63715d03568050ab71b
johnlindquist <johnlindquist@gmail.com>
2020-12-17 20:06:26 -0700

    attempting a test for share-file

--- templates/test.js
+++ templates/test.js
@@ -6,3 +6,17 @@ echo(response.data.name + " is working!")
 if (response.data.name != "simplescripts") {
   exit()
 }
+
+//testing share-file
+let testFile = "test.txt"
+await writeFile(testFile, "testing")
+let child = spawn(`share-file`, [testFile], {
+  stdio: "inherit",
+})
+
+await new Promise((res, rej) => {
+  setTimeout(res, 3000)
+})
+console.log("after timeout")
+rm(testFile)
+child.kill()
