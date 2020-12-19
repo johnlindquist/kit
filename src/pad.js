df6c15db693fb9577a5bc028b40a3f6ebc697f25
johnlindquist <johnlindquist@gmail.com>
2020-12-18 21:22:16 -0700

    adding pad

---
+++ src/pad.js
@@ -0,0 +1,81 @@
+let { watch } = await need("chokidar")
+let { default: cleanup } = await need("node-cleanup")
+
+let fileName = "scratch.js"
+let fileWrapper = "scratch-wrapper.js"
+
+let filePath = path.join(env.SIMPLE_TMP_PATH, fileName)
+let fileWrapperPath = path.join(
+  env.SIMPLE_TMP_PATH,
+  fileWrapper
+)
+
+await writeFile(filePath, "")
+edit(filePath)
+
+let execString = `
+NODE_PATH=${env.SIMPLE_PATH}/node_modules \
+DOTENV_CONFIG_PATH=${env.SIMPLE_PATH}/.env \
+${env.SIMPLE_NODE} \
+--require dotenv-with-expand/config \
+--require "${env.SIMPLE_PATH}/lib/dangerously_edit.cjs" \
+${fileWrapperPath} \
+"$@"
+`.trim()
+
+let padBreak = `//pad----------`
+
+let watcher
+let change = async (event, path, details) => {
+  await watcher.unwatch(filePath)
+  let fileContents = await readFile(filePath, "utf8")
+  let lines = fileContents
+    .split("\n")
+    .map(line => line.trim())
+  let padBreakLocation = lines.indexOf(padBreak)
+
+  let top = lines.slice(0, padBreakLocation).join("\n")
+
+  let linesToRun = lines.slice(0, padBreakLocation)
+  for (let i = linesToRun.length - 1; i > -1; i--) {
+    let line = linesToRun[i]
+    if (
+      line &&
+      !line.startsWith(" ") &&
+      !line.startsWith("//")
+    ) {
+      if (line.startsWith(";")) line = line.slice(1)
+      linesToRun.push("console.log(" + line + ")")
+      break
+    }
+  }
+
+  linesToRun = linesToRun.join("\n")
+  linesToRun = `try{${linesToRun}}catch(error){console.log(error)}`.trim()
+
+  await writeFile(fileWrapperPath, linesToRun)
+
+  let result = exec(execString, { silent: false })
+  result = result.toString()
+  if (result.startsWith("[") || result.startsWith("("))
+    result = ";" + result
+  let newContent = `
+${top}
+${padBreak}
+${result}
+`.trim()
+
+  await writeFile(filePath, newContent)
+
+  setTimeout(() => {
+    watcher.add(filePath)
+  }, 100)
+}
+
+watcher = watch(filePath)
+watcher.on("raw", change)
+
+cleanup(() => {
+  rm(filePath)
+  rm(fileWrapperPath)
+})
--- templates/test.js
+++ templates/test.js
@@ -6,3 +6,24 @@ echo(response.data.name + " is working!")
 if (response.data.name != "simplescripts") {
   exit()
 }
+echo(`successfully create a new script`)
+
+let testFile = "test.txt"
+await writeFile(testFile, "testing")
+let child = spawn(`share-file`, [testFile, "--trust"], {
+  stdio: "inherit",
+})
+
+await new Promise((res, rej) => {
+  setTimeout(res, 3000)
+})
+rm(testFile)
+kill(child.pid)
+echo(`share-file ran successfully`)
+
+child = spawn(`pad`, [testFile, "--trust", "--no-edit"], {
+  stdio: "inherit",
+})
+
+kill(child.pid)
+echo(`pad ran successfully`)
