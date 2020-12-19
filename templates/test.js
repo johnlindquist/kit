7156598e5046917acc9b7a7eca50c9d5cfd71065
johnlindquist <johnlindquist@gmail.com>
2020-12-18 22:23:39 -0700

    adding the tutorial steps to the test template

--- templates/test.js
+++ templates/test.js
@@ -8,12 +8,111 @@ echo(response.data.name + " is working!")
 if (response.data.name != "simplescripts") {
   exit()
 }
-echo(`successfully create a new script`)
+echo(`"new" passed`)
+
+//----------------------
+
+let testingTutorial = "testing-tutorial"
+let child = spawnSync(
+  `tutorial`,
+  [testingTutorial, "--trust", "--no-edit"],
+  {
+    stdio: "inherit",
+  }
+)
+
+let testingTutorialFilePath = path.join(
+  env.SIMPLE_SRC_PATH,
+  testingTutorial + ".js"
+)
+let tutorialContent = await readFile(
+  testingTutorialFilePath,
+  "utf8"
+)
+
+let tutorialContentBuffer = await readFile(
+  testingTutorialFilePath
+)
+let tutorialTemplateBuffer = await readFile(
+  path.join(env.SIMPLE_PATH, "templates", "tutorial.js")
+)
+
+if (
+  Buffer.compare(
+    tutorialContentBuffer,
+    tutorialTemplateBuffer
+  )
+) {
+  echo(`successfully used tutorial template`)
+} else {
+  echo(`tutorial content and template don't match...`)
+  echo(tutorialContent)
+  echo("-------------------")
+  echo(tutorialTemplateContent)
+  exit()
+}
+
+tutorialContent = tutorialContent.replaceAll("//", "")
+
+await writeFile(testingTutorialFilePath, tutorialContent)
+child = spawnSync(
+  testingTutorial,
+  ["johnlindquist", "--trust", "--no-edit"],
+  {
+    stdio: "inherit",
+    env: {
+      TUTORIAL_CONTENT_PATH: ".",
+      ...env,
+    },
+  }
+)
+
+echo(`"tutorial" passed`)
+
+//---------------------
+
+let { writeNewEnv } = await import(
+  path.join(env.SIMPLE_PATH, "src", "simple", "utils.js")
+)
+await writeNewEnv(
+  "TUTORIAL_CONTENT_PATH",
+  "/Users/johnlindquist/projects/blog"
+)
+
+let newDefault = "new-default"
+child = spawnSync(
+  `new`,
+  [newDefault, "--trust", "--no-edit"],
+  {
+    stdio: "inherit",
+  }
+)
+
+let newDefaultContentPath = path.join(
+  env.SIMPLE_SRC_PATH,
+  newDefault + ".js"
+)
+let newDefaultBuffer = await readFile(newDefaultContentPath)
+
+let defaultTemplateBuffer = await readFile(
+  path.join(env.SIMPLE_PATH, "templates", "default.js")
+)
+
+if (
+  Buffer.compare(newDefaultBuffer, defaultTemplateBuffer)
+) {
+  echo(
+    `"new" passed (used default template after tutorial)`
+  )
+} else {
+  echo(newDefaultContent)
+  exit()
+}
 
 let testFile = "test.txt"
 await writeFile(testFile, "testing")
 
-let child = spawn(`share-file`, [testFile, "--trust"], {
+child = spawn(`share-file`, [testFile, "--trust"], {
   stdio: "inherit",
 })
 
@@ -22,11 +121,11 @@ await new Promise((res, rej) => {
 })
 rm(testFile)
 kill(child.pid)
-echo(`share-file ran successfully`)
+echo(`"share-file" passed`)
 
 child = spawn(`pad`, [testFile, "--trust", "--no-edit"], {
   stdio: "inherit",
 })
 
 kill(child.pid)
-echo(`pad ran successfully`)
+echo(`"pad" passed`)
