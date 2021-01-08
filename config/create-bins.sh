303f9e676803e388ab46616e7d120876c32a6f4e
johnlindquist <johnlindquist@gmail.com>
2021-01-08 15:44:38 -0700

    fixing create-bins

--- config/create-bins.sh
+++ config/create-bins.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SIMPLE_NODE=$SIMPLE_PATH/node/bin/node
 
-cli=("new" "edit" "simple")
+cli=(new edit simple)
 for file in ${cli[@]}; do
     # Strip the path
     binName=${file##*/}
@@ -9,19 +9,6 @@ for file in ${cli[@]}; do
     binName=${binName%.js}
     # Create the alias
     sed "s#{{name}}#$binName#g;s#{{SIMPLE_PATH}}#$SIMPLE_PATH#g;s#{{SIMPLE_NODE}}#$SIMPLE_NODE#g;s#{{type}}#cli#g" \
-    $SIMPLE_PATH/config/template-bin > $SIMPLE_PATH/bin/$binName
-    
-    chmod +x $SIMPLE_PATH/bin/$binName
-done
-
-
-for file in $SIMPLE_PATH/scripts/*.js; do
-    # Strip the path
-    binName=${file##*/}
-    # Strip the .js extension
-    binName=${binName%.js}
-    # Create the alias
-    sed "s#{{name}}#$binName#g;s#{{SIMPLE_PATH}}#$SIMPLE_PATH#g;s#{{SIMPLE_NODE}}#$SIMPLE_NODE#g;s#{{type}}#scripts#g" \
     $SIMPLE_PATH/config/template-bin > $SIMPLE_PATH/bin/$binName
     
     chmod +x $SIMPLE_PATH/bin/$binName
