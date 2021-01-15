a8f5ace6d370e756bf94e2e8952147fd3a2a3de8
johnlindquist <johnlindquist@gmail.com>
2021-01-15 08:45:55 -0700

    adding first test through simple itself

---
+++ test/new-and-rm.js
@@ -0,0 +1,51 @@
+//https://www.npmjs.com/package/shelljs#testexpression
+
+let scriptName = "testing-new-and-rm"
+
+let scriptExists = test(
+  "-f",
+  simplePath("scripts", scriptName + ".js")
+)
+let binExists = test("-f", simplePath("bin", scriptName))
+
+if (scriptExists || binExists) {
+  console.log({ scriptExists, binExists })
+  console.log(chalk`{red Clearing out ${scriptName}}`)
+  await run("cli/remove", scriptName, "--force")
+}
+
+console.log(chalk`--- {yellow NEW} ---`)
+await run("cli/new", scriptName, "--no-edit")
+
+test("-f", simplePath("scripts", scriptName + ".js"))
+test("-f", simplePath("bin", scriptName))
+
+scriptExists = test(
+  "-f",
+  simplePath("scripts", scriptName + ".js")
+)
+binExists = test("-f", simplePath("bin", scriptName))
+
+if (!scriptExists || !binExists) {
+  console.log({ scriptExists, binExists })
+  console.log(chalk`{red Failed to create scripts}`)
+  exit(1)
+}
+
+console.log(chalk`--- {yellow RM} ---`)
+await run("cli/remove", scriptName, "--force")
+
+test("-f", simplePath("scripts", scriptName + ".js"))
+test("-f", simplePath("bin", scriptName))
+
+scriptExists = test(
+  "-f",
+  simplePath("scripts", scriptName + ".js")
+)
+binExists = test("-f", simplePath("bin", scriptName))
+
+if (scriptExists || binExists) {
+  console.log({ scriptExists, binExists })
+  console.log(chalk`{red Failed to remove scripts}`)
+  exit(1)
+}
