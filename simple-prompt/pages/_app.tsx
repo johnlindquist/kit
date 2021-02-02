12c4936b46eda576f862515b2c4a07a12483bd0f
johnlindquist <johnlindquist@gmail.com>
2021-02-02 14:07:57 -0700

    :tada: Scaffolding commit from Create Next TypeScript App

---
+++ simple-prompt/pages/_app.tsx
@@ -0,0 +1,21 @@
+
+import '../styles/globals.css'
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default App
