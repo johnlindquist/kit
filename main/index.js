a757772e41777463daefe9e687d497602687284f
johnlindquist <johnlindquist@gmail.com>
2021-03-22 22:08:08 -0600

    Fixing up the main menu

--- main/index.js
+++ main/index.js
@@ -5,20 +5,8 @@
 //Note: Feel free to edit this file!
 let { menu, scripts, validate } = await cli("fns")
 
-tab("Run", async () => {
-  await cli("run")
-})
-
-tab("Edit", async () => {
-  await cli("edit")
-})
-
-tab("New", async () => {
-  await cli("new")
-})
-
-tab("Share", async () => {
-  let script = await arg(
+const Share = async () => {
+  let cliScript = await arg(
     {
       message: `Which script do you want to share?`,
     },
@@ -40,10 +28,97 @@ tab("Share", async () => {
     },
   ])
 
-  console.log({ how, script })
-  console.log({ args })
+  await cli(how, cliScript)
+}
 
-  await cli(how, script)
-})
+const NewScript = async () => {
+  let cliScript = await arg(
+    "How would you like to create a script?",
+    [
+      {
+        name: "New from name",
+        description: "Enter a script name",
+        value: "new",
+      },
+      {
+        name: "New from template",
+        description:
+          "Enter a script name then select a template",
+        value: "new-from-template",
+      },
+      {
+        name: "New from url",
+        description: "Enter a url then name it",
+        value: "new-from-url",
+      },
+    ]
+  )
 
-tab("Settings", async () => {})
+  await cli(cliScript)
+}
+
+const Other = async () => {
+  let { host, port } = await new Promise((res, rej) => {
+    let messageHandler = data => {
+      if (data.from === "SERVER") {
+        res(data)
+        process.off("message", messageHandler)
+      }
+    }
+    process.on("message", messageHandler)
+
+    send("GET_SERVER_STATE")
+  })
+
+  let cliScript = await arg(
+    {
+      message: `Other options:`,
+    },
+    [
+      {
+        name: "Check for Update",
+        description: `Version: ${env.KIT_APP_VERSION}`,
+        value: "update",
+      },
+      {
+        name: host && port ? "Stop Server" : "Start Server",
+        description:
+          host && port
+            ? `Server running on http://${host}:${port}`
+            : "",
+        value: "toggle-server",
+      },
+      {
+        name: "Open Script Kit at Login",
+        description: "Sets Script Kit to launch at login",
+        value: "open-at-login",
+      },
+      {
+        name: "Add ~/.kenv/bin to $PATH",
+        description: `Looks for your profile and appends to $PATH`,
+        value: "add-kenv-to-profile",
+      },
+      {
+        name: "Quit",
+        description: `Quit Script Kit`,
+        value: "quit",
+      },
+    ]
+  )
+
+  await cli(cliScript)
+}
+
+const Edit = async () => {
+  await cli("edit")
+}
+
+const Run = async () => {
+  await cli("run")
+}
+
+tab("Run", Run)
+tab("Edit", Edit)
+tab("Share", Share)
+tab("New", NewScript)
+tab("Other", Other)
