61815237579682441b5d958dcb2f24784e85e8bd
John Lindquist <johnlindquist@gmail.com>
2021-08-26 08:19:11 -0600

    attempting a webhook approach

---
+++ run.js
@@ -0,0 +1,19 @@
+if (!process.env?.KIT) {
+    process.env.KIT = path.dirname(new URL(import.meta.url).pathname);
+}
+if (!process.env?.KENV) {
+    process.env.KENV = process.cwd();
+}
+import { config } from "dotenv";
+import { assignPropsTo } from "kit-bridge/esm/util";
+import "./api/global.js";
+import "./api/kit.js";
+import "./api/lib.js";
+import "./target/terminal.js";
+config({
+    path: process.env.KIT_DOTENV || kenvPath(".env"),
+});
+assignPropsTo(process.env, global.env);
+export default async (command) => {
+    await run(command);
+};
