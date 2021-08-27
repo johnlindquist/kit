f783e7d3ffee97838ee49a9b43d1c57d4108cf5a
John Lindquist <johnlindquist@gmail.com>
2021-08-27 12:56:25 -0600

    fixing postinstall scripts

--- cli/postinstall.js
+++ cli/postinstall.js
@@ -2,10 +2,5 @@ console.log(`Starting postinstall`);
 console.log(kitPath("src"));
 await trash(kitPath("src", "*"));
 let tmpScriptsPath = kitPath("tmp", "scripts");
-if (!(await isDir(tmpScriptsPath))) {
-    console.log(`Creating ${tmpScriptsPath}`);
-    mkdir("-p", tmpScriptsPath);
-}
-cp("-r", kenvPath("scripts"), tmpScriptsPath);
-console.log(kenvPath("scripts"));
+cp(kenvPath("scripts/*"), tmpScriptsPath);
 export {};
--- src/cli/postinstall.ts
+++ src/cli/postinstall.ts
@@ -1,13 +1,8 @@
 console.log(`Starting postinstall`)
 console.log(kitPath("src"))
 await trash(kitPath("src", "*"))
-let tmpScriptsPath = kitPath("tmp", "scripts")
-if (!(await isDir(tmpScriptsPath))) {
-  console.log(`Creating ${tmpScriptsPath}`)
-  mkdir("-p", tmpScriptsPath)
-}
 
-cp("-r", kenvPath("scripts"), tmpScriptsPath)
-console.log(kenvPath("scripts"))
+let tmpScriptsPath = kitPath("tmp", "scripts")
+cp(kenvPath("scripts/*"), tmpScriptsPath)
 
 export {}
