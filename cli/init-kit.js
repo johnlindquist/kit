a014bd56a5162e8fa204294c18e503849330640b
John Lindquist <johnlindquist@gmail.com>
2021-08-28 07:24:09 -0600

    kit

--- cli/init-kit.js
+++ cli/init-kit.js
@@ -3,7 +3,7 @@ if (removeSrc) {
     await trash([kitPath("src")]);
 }
 let kitScriptsPath = kitPath("scripts");
-cp(kenvPath("scripts"), kitScriptsPath);
+cp(kenvPath("scripts/*"), kitScriptsPath);
 let scripts = await readdir(kitScriptsPath);
 let scriptImports = scripts
     .filter(s => s.endsWith(".js"))
--- src/cli/init-kit.ts
+++ src/cli/init-kit.ts
@@ -5,7 +5,7 @@ if (removeSrc) {
 
 let kitScriptsPath = kitPath("scripts")
 
-cp(kenvPath("scripts"), kitScriptsPath)
+cp(kenvPath("scripts/*"), kitScriptsPath)
 let scripts = await readdir(kitScriptsPath)
 let scriptImports = scripts
   .filter(s => s.endsWith(".js"))
