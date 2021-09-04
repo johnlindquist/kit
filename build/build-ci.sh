0acd55fb4d130d921f131385138ee72f4b8d6a61
John Lindquist <johnlindquist@gmail.com>
2021-09-04 17:13:17 -0600

    fix(build): testing semantic release

--- build/build-ci.sh
+++ build/build-ci.sh
@@ -6,11 +6,13 @@ echo $WD
 
 rm -r $KIT 2> /dev/null
 mkdir -p $KIT
-npx tsc --outDir $KIT
 cp -a root/. $KIT
 cp *.md *.json LICENSE $KIT
 
 ./build/install-node.sh --prefix $KIT/node
+PATH=./node/bin npm i
+
+npx tsc --outDir $KIT
 
 cd $KIT
 PATH=./node/bin npm i --production
