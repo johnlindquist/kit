de842d53d77b91958f47e09f50458fc276366327
John Lindquist <johnlindquist@gmail.com>
2021-09-06 09:32:42 -0600

    fix(build): testing semantic release

--- build/build-ci.sh
+++ build/build-ci.sh
@@ -4,4 +4,4 @@ export WD=$(cd "$(dirname ${BASH_SOURCE[0]})"/.. &> /dev/null && pwd)
 export KIT="$WD/dist"
 export PATH="$KIT/node/bin:$PATH"
 
-"$WD/build/build-base.sh"
+PATH=$PATH "$WD/build/build-base.sh"
