15ed9f47ffe16d02653385e52b1f11de857dc214
John Lindquist <johnlindquist@gmail.com>
2021-09-06 09:40:49 -0600

    fix(build): testing semantic release

--- build/build-ci.sh
+++ build/build-ci.sh
@@ -2,6 +2,22 @@ set -e
 
 export WD=$(cd "$(dirname ${BASH_SOURCE[0]})"/.. &> /dev/null && pwd)
 export KIT="$WD/dist"
-export PATH="$KIT/node/bin:$PATH"
+export PATH="$WD/node/bin:$PATH"
 
-PATH=$PATH "$WD/build/build-base.sh"
+cd $WD
+echo $WD
+
+mkdir -p $KIT
+cp -a root/. $KIT
+./build/install-node.sh --prefix $KIT/node
+cp -r types $KIT
+cp *.md package*.json LICENSE $KIT
+
+npm i --only=dev
+npx tsc --outDir $KIT
+npx tsc --project ./tsconfig-cjs.json --outDir "$KIT/cjs"
+node ./scripts/fix-cjs.js
+
+cd $KIT
+echo $KIT
+npm i --production
