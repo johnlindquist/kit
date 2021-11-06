cd0a89031264760f30a665695344997c9cf0ee5c
John Lindquist <johnlindquist@gmail.com>
2021-11-06 07:54:44 -0600

    fix(downloads): handle errors

--- src/setup/downloads.ts
+++ src/setup/downloads.ts
@@ -1,4 +1,8 @@
-await run(kitPath("hot", "download-hot.js"))
-await run(kitPath("help", "download-docs.js"))
+try {
+  await run(kitPath("hot", "download-hot.js"))
+  await run(kitPath("help", "download-docs.js"))
+} catch {
+  console.warn(`Failed to download data`)
+}
 
 export {}
