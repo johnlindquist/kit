374b8402a5ff1c4b7f9a9137380e3e5fcef7b9b6
John Lindquist <johnlindquist@gmail.com>
2022-08-09 10:05:46 -0600

    fix(actionbar): styles, features, fixes

--- src/cli/share.ts
+++ src/cli/share.ts
@@ -5,42 +5,48 @@ import { CLI } from "../cli"
 
 let { filePath } = await selectScript(`Share which script?`)
 
-let how: keyof CLI = await arg("Select a Share Option", [
+let how: keyof CLI = await arg(
   {
-    name: "Create Github Discussion",
-    description:
-      "Post script as gist and open Github discussion in browser",
-    value: "share-script-as-discussion",
+    placeholder: "Share",
+    resize: false,
   },
-  {
-    name: "Copy",
-    description: "Copy the script to the clipboard",
-    value: "share-copy",
-  },
-  {
-    name: "Copy as Markdown",
-    description:
-      "Copies script contents in fenced JS Markdown",
-    value: "share-script-as-markdown",
-  },
-  {
-    name: "Post as a gist",
-    description: "Post the script as a gist",
-    value: "share-script",
-  },
-  {
-    name: "Create Install URL",
-    description:
-      "Create a link which will install the script",
-    value: "share-script-as-link",
-  },
-  {
-    name: "Share as kit:// link",
-    description:
-      "Create a link which will install the script",
-    value: "share-script-as-kit-link",
-  },
-])
+  [
+    {
+      name: "Create Github Discussion",
+      description:
+        "Post script as gist and open Github discussion in browser",
+      value: "share-script-as-discussion",
+    },
+    {
+      name: "Copy",
+      description: "Copy the script to the clipboard",
+      value: "share-copy",
+    },
+    {
+      name: "Copy as Markdown",
+      description:
+        "Copies script contents in fenced JS Markdown",
+      value: "share-script-as-markdown",
+    },
+    {
+      name: "Post as a gist",
+      description: "Post the script as a gist",
+      value: "share-script",
+    },
+    {
+      name: "Create Install URL",
+      description:
+        "Create a link which will install the script",
+      value: "share-script-as-link",
+    },
+    {
+      name: "Share as kit:// link",
+      description:
+        "Create a link which will install the script",
+      value: "share-script-as-kit-link",
+    },
+  ]
+)
 
 await cli(how, filePath)
 
