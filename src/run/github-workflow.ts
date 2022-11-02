dcb06025ed03653b5c6928c42c4c3334735eec1a
John Lindquist <johnlindquist@gmail.com>
2022-11-02 14:01:44 -0600

    fix(github): trying to exit on errors

--- src/run/github-workflow.ts
+++ src/run/github-workflow.ts
@@ -1,18 +1,20 @@
 import os from "os"
-import { configEnv } from "../core/utils.js"
+import {
+  configEnv,
+  resolveToScriptPath,
+} from "../core/utils.js"
 
 await import("../api/global.js")
 await import("../api/kit.js")
 await import("../api/lib.js")
 
 let platform = process.env?.PLATFORM || os.platform()
-try {
-  await import(`../platform/${platform}.js`)
-} catch (error) {
-  // console.log(`No ./platform/${platform}.js`)
-}
+
+await import(`../platform/${platform}.js`)
 
 configEnv()
 
 await import("../target/terminal.js")
-await import(await arg("Path to script"))
+await import(
+  resolveToScriptPath(await arg("Path to script"))
+)
