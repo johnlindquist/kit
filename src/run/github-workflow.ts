eff73995da5d1ebfc8f62923122e9b21a908755f
John Lindquist <johnlindquist@gmail.com>
2022-11-02 13:50:11 -0600

    fix(github): trying to exit on errors

--- src/run/github-workflow.ts
+++ src/run/github-workflow.ts
@@ -5,21 +5,6 @@ await import("../api/global.js")
 await import("../api/kit.js")
 await import("../api/lib.js")
 
-process.on("unhandledRejection", (reason, promise) => {
-  console.error(
-    "Unhandled Rejection at:",
-    promise,
-    "reason:",
-    reason
-  )
-  process.exitCode = 1
-})
-
-process.on("uncaughtException", error => {
-  console.error("Uncaught Exception:", error)
-  process.exitCode = 1
-})
-
 let platform = process.env?.PLATFORM || os.platform()
 try {
   await import(`../platform/${platform}.js`)
@@ -30,11 +15,4 @@ try {
 configEnv()
 
 await import("../target/terminal.js")
-let { runCli } = await import("../cli/kit.js")
-
-try {
-  await runCli()
-} catch (error) {
-  console.log(error)
-  process.exitCode = 1
-}
+await import(await arg("Path to script"))
