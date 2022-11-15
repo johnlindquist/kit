25f776b0be112de1d71b5a58b9ea923732c77152
John Lindquist <johnlindquist@gmail.com>
2022-11-15 01:42:26 -0700

    fix(examples): stash changes

--- src/setup/clone-examples.ts
+++ src/setup/clone-examples.ts
@@ -1,7 +1,9 @@
 let examplesDir = kenvPath("kenvs", "examples")
 if (await isDir(examplesDir)) {
   cd(examplesDir)
-  await exec(`git pull --rebase --autostash --stat`)
+
+  await exec(`git stash`)
+  await exec(`git pull`)
 } else {
   cd(kenvPath("kenvs"))
   await exec(
