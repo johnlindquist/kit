074ab05b9b27975d695bf575c6d727616213d63d
John Lindquist <johnlindquist@gmail.com>
2022-12-20 17:02:18 -0700

    fix(github): workflow use absolute

--- src/run/github-workflow.ts
+++ src/run/github-workflow.ts
@@ -1,24 +1,30 @@
 import os from "os"
+import { pathToFileURL } from "url"
 import {
   configEnv,
   resolveToScriptPath,
 } from "../core/utils.js"
 
-await import("../api/global.js")
-await import("../api/kit.js")
-await import("../api/lib.js")
+let relativeImport = async (filePath: string) =>
+  await import(
+    pathToFileURL(filePath).href + "?uuid=" + global.uuid()
+  )
+
+await relativeImport("../api/global.js")
+await relativeImport("../api/kit.js")
+await relativeImport("../api/lib.js")
 
 let platform = process.env?.PLATFORM || os.platform()
 
-await import(`../platform/${platform}.js`)
+await relativeImport(`../platform/${platform}.js`)
 
 configEnv()
 
-await import("../target/terminal.js")
+await relativeImport("../target/terminal.js")
 
 global.core = await npm("@actions/core")
 global.github = await npm("@actions/github")
 
-await import(
+await relativeImport(
   resolveToScriptPath(await arg("Path to script"))
 )
