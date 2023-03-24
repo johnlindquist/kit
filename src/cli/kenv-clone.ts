79123582c9c9ef051f2235654c7625fff6152553
John Lindquist <johnlindquist@gmail.com>
2023-03-24 14:45:07 -0600

    fix(kenv): built ts scripts after clone

--- src/cli/kenv-clone.ts
+++ src/cli/kenv-clone.ts
@@ -1,44 +1,40 @@
 //Description: Clone a Kenv repo
-
 let kenvsDir = kenvPath("kenvs")
-
 if (!(await isDir(kenvsDir))) {
   await ensureDir(kenvsDir)
 }
-
 // TODO: Better example kenvs
-
-let panel = md(`## Full url or \`user/repo\` for github
-
-## Examples
+let panel = md(
+  `
+# Examples
 * [\`johnlindquist/kit-examples\`](submit:johnlindquist/kit-examples)
 * [\`https://github.com/johnlindquist/kit-examples\`](submit:https://github.com/johnlindquist/kit-examples)
 
-`)
+`.trim()
+)
 // TODO: Move to a single command
-let repo = await arg({
-  placeholder: `Enter url to kenv repo`,
-  ignoreBlur: true,
-  panel,
-})
-
+let repo = await arg(
+  {
+    placeholder: `Enter url to kenv repo`,
+    hint: `Enter url (or user/repo) to kenv repo`,
+    ignoreBlur: true,
+    resize: true,
+  },
+  panel
+)
 if (repo?.match(/^[^\/|:]*\/[^\/]*$/)) {
   repo = `https://github.com/${repo}`
 }
-
 let input =
   repo.replace(/\.git$/, "").match(/(?<=\/)[^\/]+$/)[0] ||
   ""
-
 let validate = async input => {
   let exists = await isDir(kenvPath("kenvs", input))
   if (exists) {
     return `${input} already exists`
   }
-
   return true
 }
-
 let onInput = async input => {
   let exists = await isDir(kenvPath("kenvs", input))
   let panel = !input
@@ -49,20 +45,18 @@ let onInput = async input => {
 \`${kenvPath("kenvs", input)}\``)
   setPanel(panel)
 }
-
 let kenvName = await arg({
   placeholder: `Enter a kenv name`,
+  hint: `Enter a directory name (defaults to repo name)`,
   input,
+  resize: true,
+  strict: false,
   validate,
   onInput,
 })
-
 let kenvDir = kenvPath("kenvs", kenvName)
-
 await exec(`git clone ${repo} ${kenvDir}`)
-
 await cli("create-all-bins")
-
+await run(kitPath("setup", "build-ts-scripts.js"))
 await mainScript()
-
 export {}
