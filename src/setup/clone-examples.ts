a3587e7a10349aaaeb915281e15fff80ee7c9cb4
John Lindquist <johnlindquist@gmail.com>
2023-05-01 17:41:47 -0600

    fix(examples): download examples if no git

--- src/setup/clone-examples.ts
+++ src/setup/clone-examples.ts
@@ -1,14 +1,49 @@
+import fs from "fs/promises"
+
 let examplesDir = kenvPath("kenvs", "examples")
 if (await isDir(examplesDir)) {
   cd(examplesDir)
 
-  await exec(`git stash`)
-  await exec(`git pull`)
+  if (isDir(kenvPath("kenvs", "examples", ".git"))) {
+    await exec(`git stash`)
+    await exec(`git pull`)
+  }
 } else {
   cd(kenvPath("kenvs"))
-  await exec(
-    `git clone https://github.com/johnlindquist/kit-examples examples`
-  )
+  if (await isBin("git")) {
+    await exec(
+      `git clone https://github.com/johnlindquist/kit-examples examples`
+    )
+  } else {
+    let destination = kenvPath("tmp")
+    await download(
+      `https://github.com/johnlindquist/kit-examples/archive/main.zip`,
+      destination,
+      {
+        extract: true,
+      }
+    )
+
+    // move diretory destination to kenvPath("kenvs", "kit-examples")
+    let source = path.join(destination, "kit-examples-main")
+    let newDestination = kenvPath("kenvs", "examples")
+    await fs.rename(source, newDestination)
+
+    // find the first file in the new destination and touch it
+    let files = await fs.readdir(
+      path.join(newDestination, "scripts")
+    )
+    let firstFile = path.join(
+      newDestination,
+      "scripts",
+      files[0]
+    )
+    // if firstFile exists, touch it
+    if (await fs.stat(firstFile)) {
+      await wait(1000)
+      await fs.utimes(firstFile, new Date(), new Date())
+    }
+  }
 }
 
 export {}
