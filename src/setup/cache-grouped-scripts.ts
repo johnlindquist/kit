40a3f041139d220b9c219b63303ea1529d1acb8e
John Lindquist <johnlindquist@gmail.com>
2023-07-15 09:30:37 -0600

    fix(cache): remove file caching

--- src/setup/cache-grouped-scripts.ts
+++ src/setup/cache-grouped-scripts.ts
@@ -1,20 +1,14 @@
 // Description: Clear Timestamps
 
 import { getGroupedScripts } from "../api/kit.js"
-import {
-  getCachePath,
-  mainScriptPath,
-  formatChoices,
-} from "../core/utils.js"
+import { formatChoices } from "../core/utils.js"
 
-let cachePath = getCachePath(mainScriptPath, "choices")
 let groupedScripts = await getGroupedScripts()
 let scripts = formatChoices(groupedScripts).map(choice => {
   choice.description = ""
   return choice
 })
 
-await ensureDir(path.dirname(cachePath))
-await writeJson(cachePath, scripts)
+process.send(scripts)
 
 export {}
