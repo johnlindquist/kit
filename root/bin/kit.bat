c4ff61f684c0d66aa3a259856e2d1bb6dbc9ce35
John Lindquist <johnlindquist@gmail.com>
2023-07-22 22:26:07 -0600

    fix(windows): kit.bat

--- root/bin/kit.bat
+++ root/bin/kit.bat
@@ -8,16 +8,9 @@ set "KIT=%~dp0.."
 REM Check if the script file is in the "node_modules" directory
 echo %~dp0 | findstr /C:"node_modules" >nul
 if %errorlevel%==0 (
-    REM Change the directory to the parent of the script file
-    cd /d "%~dp0.."
-    set "KIT=%CD%/@johnlindquist/kit"
-
     REM Set KENV variable to the grandparent directory of the script file
     cd /d "%~dp0../.."
     set "KENV=%CD%"
-) else (
-    REM Set KIT to the current script directory
-    set "KIT=%~dp0"
 )
 
 REM Set the default KIT_NODE variable to the custom node binary
