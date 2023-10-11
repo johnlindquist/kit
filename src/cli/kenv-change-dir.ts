8c99b01c6e77327820f8212a1cdc92d8b2c74ce1
John Lindquist <johnlindquist@gmail.com>
2023-10-10 22:03:12 -0700

    fix(upgrade): upgrading packages, kenv-change-dir move options

--- src/cli/kenv-change-dir.ts
+++ src/cli/kenv-change-dir.ts
@@ -4,13 +4,29 @@ export {}
 
 // TODO: Testing opening app from Alfred/Terminal
 // TODO: Configure "move" kenv or "create" new kenv in directory
-let newKenvPath = await path({
+let newKenvParentPath = await path({
   hint: `Select the parent directory that will contain your .kenv`,
 })
 
+let moveKenv = await arg("Move ~/.kenv to directory?", [
+  "no",
+  "yes",
+])
+if (moveKenv === "yes") {
+  await move(
+    kenvPath(),
+    path.join(newKenvParentPath, ".kenv")
+  )
+  await replace({
+    files: [path.join(newKenvParentPath, ".kenv", ".env")],
+    from: /KENV=.*/,
+    to: `KENV=${path.join(newKenvParentPath, ".kenv")}`,
+  })
+}
+
 await sendWait(
   Channel.KENV_NEW_PATH,
-  path.join(newKenvPath, ".kenv")
+  path.join(newKenvParentPath, ".kenv")
 )
 
 await div({
