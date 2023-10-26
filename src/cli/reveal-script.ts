11dc6a16fca07b029913f6e955fe4c522342ba6d
John Lindquist <johnlindquist@gmail.com>
2023-10-26 17:41:51 -0600

    fix(reveal): change reveal script to use revealFile

--- src/cli/reveal-script.ts
+++ src/cli/reveal-script.ts
@@ -1,12 +1,8 @@
 // Description: Reveal the selected script in Finder
 
 let { filePath } = await selectScript(
-  `Which script do you want to duplicate?`
+  `Which script do you want to reveal?`
 )
-await open(path.dirname(filePath))
-await applescript(`
-set aFile to (POSIX file "${filePath}") as alias
-tell application "Finder" to select aFile
-`)
+await revealFile(filePath)
 
 export {}
