ec49f1e29e5823af4bd758f7cc59493b6819c8e4
John Lindquist <johnlindquist@gmail.com>
2024-02-18 23:30:52 -0700

    fix(npm): publish next on npm

--- src/api/kit.test.js
+++ src/api/kit.test.js
@@ -48,18 +48,3 @@ ava.serial(`tmpPath generates a tmp path`, async t => {
   )
   t.is(stderr, "")
 })
-
-ava.serial(`npm installs a package`, async t => {
-  let script = `mock-npm-install-express`
-
-  let { stdout, stderr } = await testScript(
-    script,
-    `
-    await npm("express")
-    `
-  )
-  let pkg = await readJson(kenvPath("package.json"))
-
-  t.truthy(pkg.devDependencies?.["express"])
-  t.falsy(pkg.dependencies?.["express"])
-})
