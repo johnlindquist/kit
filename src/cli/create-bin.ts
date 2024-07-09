4574bcd55ee0ba89041f7b05cd8ed78df656ecf0
John Lindquist <johnlindquist@gmail.com>
2024-07-09 09:12:16 -0600

    fix(scriptlets): support args in terminal scriptlet bins

--- src/cli/create-bin.ts
+++ src/cli/create-bin.ts
@@ -1,14 +1,7 @@
 import { Bin } from "../core/enum.js"
 import { createBinFromScript } from "./lib/utils.js"
 
-let type = await arg<Bin>(
-  "Select type:",
-  Object.values(Bin)
-)
+let type = await arg<Bin>("Select type:", Object.values(Bin))
 
-let script = await selectScript(
-  `Create bin from which script?`,
-  false
-)
+let script = await selectScript("Create bin from which script?", false)
 await createBinFromScript(type, script)
-export {}
