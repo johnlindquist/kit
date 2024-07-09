a9b5c647476c69173a426a8917495ce713fe9e80
John Lindquist <johnlindquist@gmail.com>
2024-07-09 09:19:29 -0600

    fix(test): fixing kenv.test.js

--- src/cli/kenv.test.js
+++ src/cli/kenv.test.js
@@ -1,19 +1,25 @@
 import ava from "ava"
 import "../../test-sdk/config.js"
 
-let kenvName = `mock-kenv`
-ava.serial("kenv create", async t => {
-  await $`KIT_MODE=js kit kenv-create ${kenvName}`
+let kenvName = "mock-kenv"
+ava.serial("kenv create", async (t) => {
+	let { stdout } = await $`KIT_MODE=js kit kenv-create ${kenvName}`
 
-  t.log(await readdir(kenvPath("kenvs")))
+	t.log({ stdout })
 
-  t.true(await pathExists(kenvPath("kenvs", kenvName)))
+	let kenvs = await readdir(kenvPath("kenvs"))
+	let kenvExists = await pathExists(kenvPath("kenvs", kenvName))
+	t.log({ kenvs, kenvExists })
+
+	t.true(kenvExists)
 })
 
-ava.serial("kenv remove", async t => {
-  await $`KIT_MODE=js kit kenv-rm ${kenvName}`
+ava.serial("kenv remove", async (t) => {
+	await $`KIT_MODE=js kit kenv-rm ${kenvName}`
 
-  t.log(await readdir(kenvPath("kenvs")))
+	let kenvs = await readdir(kenvPath("kenvs"))
+	let kenvExists = await pathExists(kenvPath("kenvs", kenvName))
+	t.log({ kenvs, kenvExists })
 
-  t.false(await pathExists(kenvPath("kenvs", kenvName)))
+	t.false(kenvExists)
 })
