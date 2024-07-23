3ae388eaf63ecc01acfcab0a994107da7bda89bf
John Lindquist <johnlindquist@gmail.com>
2024-07-23 12:32:56 -0600

    fix(windows): shortcuts test

--- src/core/shortcuts.test.ts
+++ src/core/shortcuts.test.ts
@@ -1,94 +1,53 @@
-import ava from "ava"
+import ava, { type ExecutionContext } from "ava"
 import { friendlyShortcut, shortcutNormalizer } from "./utils"
 
-ava("no change", async (t) => {
-	const beforeFriendly = "cmd+e"
-	const afterFriendlyMac = "cmd+e"
-	const afterFriendlyWindows = "ctrl+e"
-	const friendlyResult = friendlyShortcut(shortcutNormalizer(beforeFriendly))
+function testShortcut(
+	t: ExecutionContext,
+	beforeFriendly: string,
+	afterFriendlyMac: string,
+	afterFriendlyWindows: string,
+	skipNormalization = false
+) {
+	const friendlyResult = skipNormalization
+		? friendlyShortcut(beforeFriendly)
+		: friendlyShortcut(shortcutNormalizer(beforeFriendly))
 
-	if (process.platform === "win32") {
-		t.is(friendlyResult, afterFriendlyWindows)
-		t.not(friendlyResult, afterFriendlyMac)
-	} else {
+	if (process.platform === "darwin") {
 		t.is(friendlyResult, afterFriendlyMac)
 		t.not(friendlyResult, afterFriendlyWindows)
+	} else {
+		t.is(friendlyResult, afterFriendlyWindows)
+		t.not(friendlyResult, afterFriendlyMac)
 	}
+}
+
+ava("no change", async (t) => {
+	testShortcut(t, "cmd+e", "cmd+e", "ctrl+e")
 })
 
 ava("parse simple", async (t) => {
-	const beforeFriendly = "cmd e"
-	const afterFriendlyMac = "cmd+e"
-	const afterFriendlyWindows = "ctrl+e"
-	const friendlyResult = friendlyShortcut(shortcutNormalizer(beforeFriendly))
-
-	if (process.platform === "win32") {
-		t.is(friendlyResult, afterFriendlyWindows)
-		t.not(friendlyResult, afterFriendlyMac)
-	} else {
-		t.is(friendlyResult, afterFriendlyMac)
-		t.not(friendlyResult, afterFriendlyWindows)
-	}
+	testShortcut(t, "cmd e", "cmd+e", "ctrl+e")
 })
 
 ava("parse with spaces", async (t) => {
-	const beforeFriendly = "cmd shift e"
-	const afterFriendlyMac = "cmd+shift+e"
-	const afterFriendlyWindows = "ctrl+shift+e"
-	const friendlyResult = friendlyShortcut(shortcutNormalizer(beforeFriendly))
-
-	if (process.platform === "win32") {
-		t.is(friendlyResult, afterFriendlyWindows)
-		t.not(friendlyResult, afterFriendlyMac)
-	} else {
-		t.is(friendlyResult, afterFriendlyMac)
-		t.not(friendlyResult, afterFriendlyWindows)
-	}
+	testShortcut(t, "cmd shift e", "cmd+shift+e", "ctrl+shift+e")
 })
 
 ava("parse with +", async (t) => {
-	const beforeFriendly = "cmd+shift+e"
-	const afterFriendlyMac = "cmd+shift+e"
-	const afterFriendlyWindows = "ctrl+shift+e"
-	const friendlyResult = friendlyShortcut(shortcutNormalizer(beforeFriendly))
-
-	if (process.platform === "win32") {
-		t.is(friendlyResult, afterFriendlyWindows)
-		t.not(friendlyResult, afterFriendlyMac)
-	} else {
-		t.is(friendlyResult, afterFriendlyMac)
-		t.not(friendlyResult, afterFriendlyWindows)
-	}
+	testShortcut(t, "cmd+shift+e", "cmd+shift+e", "ctrl+shift+e")
 })
 
 ava("parse out of order", async (t) => {
-	const beforeFriendly = "shift cmd e"
-	const afterFriendlyMac = "cmd+shift+e"
-	const afterFriendlyWindows = "ctrl+shift+e"
-	const friendlyResult = friendlyShortcut(shortcutNormalizer(beforeFriendly))
-
-	if (process.platform === "win32") {
-		t.is(friendlyResult, afterFriendlyWindows)
-		t.not(friendlyResult, afterFriendlyMac)
-	} else {
-		t.is(friendlyResult, afterFriendlyMac)
-		t.not(friendlyResult, afterFriendlyWindows)
-	}
+	testShortcut(t, "shift cmd e", "cmd+shift+e", "ctrl+shift+e")
+	testShortcut(t, "shift opt e", "opt+shift+e", "alt+shift+e")
+})
+ava("parse caps", async (t) => {
+	testShortcut(t, "Shift Cmd E", "cmd+shift+e", "ctrl+shift+e")
+	testShortcut(t, "Shift Command E", "cmd+shift+e", "ctrl+shift+e")
 })
 
 ava("parse alt", async (t) => {
-	const beforeFriendly = "alt cmd e"
-	const afterFriendlyMac = "cmd+opt+e"
-	const afterFriendlyWindows = "ctrl+alt+e"
-	const friendlyResult = friendlyShortcut(shortcutNormalizer(beforeFriendly))
-
-	if (process.platform === "win32") {
-		t.is(friendlyResult, afterFriendlyWindows)
-		t.not(friendlyResult, afterFriendlyMac)
-	} else {
-		t.is(friendlyResult, afterFriendlyMac)
-		t.not(friendlyResult, afterFriendlyWindows)
-	}
+	testShortcut(t, "alt cmd e", "cmd+opt+e", "ctrl+alt+e")
 })
 
 ava("friendlyShortcut requires normalization", async (t) => {
@@ -97,9 +56,9 @@ ava("friendlyShortcut requires normalization", async (t) => {
 	const afterFriendlyWindows = "ctrl+shift+e"
 	const friendlyResult = friendlyShortcut(beforeFriendly)
 	// Friendly shortcuts require normalization prior to parsing
-	if (process.platform === "win32") {
-		t.not(friendlyResult, afterFriendlyWindows)
-	} else {
+	if (process.platform === "darwin") {
 		t.not(friendlyResult, afterFriendlyMac)
+	} else {
+		t.not(friendlyResult, afterFriendlyWindows)
 	}
 })
