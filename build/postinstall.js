d85a05f3acff1fec134f8202ae6df1c4e528cac6
johnlindquist <johnlindquist@gmail.com>
2024-07-22 20:58:08 -0600

    fix(build): postinstall

--- build/postinstall.js
+++ build/postinstall.js
@@ -3,17 +3,24 @@ import { rimraf } from "rimraf"
 import { homedir, platform } from "node:os"
 import { existsSync } from "node:fs"
 
+let isCI = process.env.CI === "true"
+if (isCI) {
+	console.log("Running in CI, skipping postinstall")
+} else {
+	let kitPath = (...pathParts) =>
+		path.resolve(
+			process.env.KIT || path.resolve(homedir(), ".kit"),
+			...pathParts
+		)
 
-let kitPath = (...pathParts) =>
-	path.resolve(process.env.KIT || path.resolve(homedir(), ".kit"), ...pathParts)
+	console.log(`Run postinstall on ${kitPath()}`)
 
-console.log(`Run postinstall on ${kitPath()}`)
+	let binFileToRemove = platform() === "win32" ? "kit" : "kit.bat"
 
-let binFileToRemove = platform() === "win32" ? "kit" : "kit.bat"
-
-if (!process.env.CI) {
-    if (existsSync(kitPath("bin", binFileToRemove))) {
-        console.log(`Removing ${kitPath("bin", binFileToRemove)} so it doesn't interfere with kit.bat`)
-        await rimraf(kitPath("bin", binFileToRemove))
-    }
+	if (existsSync(kitPath("bin", binFileToRemove))) {
+		console.log(
+			`Removing ${kitPath("bin", binFileToRemove)} so it doesn't interfere with kit.bat`
+		)
+		await rimraf(kitPath("bin", binFileToRemove))
+	}
 }
