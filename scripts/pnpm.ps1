9aa563118c2b8bc0bb745de3e88cead4b4a551a9
John Lindquist <johnlindquist@gmail.com>
2024-10-10 19:15:59 -0700

    fix(pnpm): publish install scripts

---
+++ scripts/pnpm.ps1
@@ -0,0 +1,152 @@
+#!/usr/bin/env pwsh
+
+# Stop executing script on any error
+$ErrorActionPreference = 'Stop'
+# Do not show download progress
+$ProgressPreference = 'SilentlyContinue'
+
+# Taken from https://stackoverflow.com/a/34559554/6537420
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+$platform = $null
+$architecture = $null
+$pnpmName = $null
+
+# PowerShell versions before 6.* were only for Windows OS
+if ($PSVersionTable.PSVersion.Major -eq 5) {
+  $platform = 'win'
+}
+
+if ($PSVersionTable.PSVersion.Major -ge 6) {
+  if ($PSVersionTable.Platform -eq 'Unix') {
+    switch -Wildcard ($PSVersionTable.OS) {
+      'Darwin*' {
+        $platform = 'macos'
+      }
+      'Linux*' {
+        $platform = 'linux'
+      }
+      'Ubuntu*' {
+        $platform = 'linux'
+      }
+    }
+
+    # PowerShell does not seem to have normal cmdlets for retrieving system information, so we use UNAME(1) for this.
+    $arch = uname -m
+    switch -Wildcard ($arch) {
+      'x86_64' { $architecture = 'x64'; Break }
+      'amd64' { $architecture = 'x64'; Break }
+      'armv*' { $architecture = 'arm'; Break }
+      'arm64' { $architecture = 'arm64'; Break }
+      'aarch64' { $architecture = 'arm64'; Break }
+    }
+
+    # 'uname -m' in some cases mis-reports 32-bit OS as 64-bit, so double check
+    if ([System.Environment]::Is64BitOperatingSystem -eq $false) {
+      if ($architecture -eq 'x64') {
+        $architecture = 'i686'
+      }
+
+      if ($architecture -eq 'arm64') {
+        $architecture = 'arm'
+      }
+    }
+
+    $pnpmName = "pnpm"
+  }
+
+  if ($PSVersionTable.Platform -eq 'Win32NT') {
+    $platform = 'win'
+  }
+}
+
+if ($platform -eq 'win') {
+  if ([System.Environment]::Is64BitOperatingSystem -eq $true) {
+    $architecture = 'x64'
+  }
+
+  if ([System.Environment]::Is64BitOperatingSystem -eq $false) {
+    $architecture = 'i686'
+  }
+
+  $pnpmName = "pnpm.exe"
+}
+
+if ($null -eq $platform) {
+  Write-Error "Platform could not be determined! Only Windows, Linux and MacOS are supported."
+}
+
+switch ($architecture) {
+  'x64' { ; Break }
+  'arm64' { ; Break }
+  Default {
+    Write-Error "Sorry! pnpm currently only provides pre-built binaries for x86_64/arm64 architectures."
+  }
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$pkgInfo = Invoke-WebRequest "https://registry.npmjs.org/@pnpm/exe" -UseBasicParsing
+$versionJson = $pkgInfo.Content | ConvertFrom-Json
+$versions = Get-Member -InputObject $versionJson.versions -Type NoteProperty | Select-Object -ExpandProperty Name
+$distTags = Get-Member -InputObject $versionJson.'dist-tags' -Type NoteProperty | Select-Object -ExpandProperty Name
+
+$version = $null
+$preferredVersion = "latest"
+
+if ($null -ne $env:PNPM_VERSION -and $env:PNPM_VERSION -ne "") {
+  $preferredVersion = $env:PNPM_VERSION
+}
+
+if ($null -eq $version -and $preferredVersion -in $distTags) {
+  $version = $versionJson.'dist-tags' | Select-Object -ExpandProperty $preferredVersion
+}
+
+if ($null -eq $version -and $preferredVersion -in $versions) {
+  $version = $preferredVersion
+}
+
+if ($null -eq $version) {
+  Write-Host "Current tags:" -ForegroundColor Yellow -NoNewline
+  $versionJson.'dist-tags' | Format-List
+
+  Write-Host "Versions:" -ForegroundColor Yellow -NoNewline
+  $versionJson.versions | Get-Member -Type NoteProperty | Format-Wide -Property Name -AutoSize
+
+  Write-Error "Sorry! pnpm '$preferredVersion' version could not be found. Use one of the tags or published versions from the provided list"
+}
+
+Write-Host "Downloading pnpm from GitHub...`n" -ForegroundColor Green
+
+$tempFileFolder = New-TemporaryDirectory
+$tempFile = (Join-Path $tempFileFolder.FullName $pnpmName)
+$archiveUrl="https://github.com/pnpm/pnpm/releases/download/v$version/pnpm-$platform-$architecture"
+if ($platform -eq 'win') {
+  $archiveUrl="$archiveUrl.exe"
+}
+Invoke-WebRequest $archiveUrl -OutFile $tempFile -UseBasicParsing
+
+Write-Host "Running setup...`n" -ForegroundColor Green
+
+if ($platform -ne 'win') {
+  chmod +x $tempFile
+}
+
+$PNPM_HOME = Join-Path $env:USERPROFILE ".kitpnpm"
+$env:PNPM_HOME = $PNPM_HOME
+
+$targetDir = $PNPM_HOME
+$newExecPath = Join-Path $targetDir (Split-Path $tempFile -Leaf)
+
+if ((Resolve-Path $newExecPath) -ne (Resolve-Path $tempFile)) {
+    Write-Host "Copying pnpm CLI from $tempFile to $newExecPath"
+    New-Item -ItemType Directory -Force -Path $targetDir | Out-Null
+    Copy-Item $tempFile $newExecPath -Force
+}
+
+Remove-Item $tempFile
+Remove-Item $tempFileFolder -Recurse
---
+++ scripts/pnpm.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+
+# From https://github.com/Homebrew/install/blob/master/install.sh
+abort() {
+  printf "%s\n" "$@"
+  exit 1
+}
+
+# string formatters
+if [ -t 1 ]; then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_blue="$(tty_mkbold 34)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
+ohai() {
+  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$1"
+}
+
+# End from https://github.com/Homebrew/install/blob/master/install.sh
+
+download() {
+  if command -v curl > /dev/null 2>&1; then
+    curl -fsSL "$1"
+  else
+    wget -qO- "$1"
+  fi
+}
+
+is_glibc_compatible() {
+  getconf GNU_LIBC_VERSION >/dev/null 2>&1 || ldd --version >/dev/null 2>&1 || return 1
+}
+
+detect_platform() {
+  local platform
+  platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+  case "${platform}" in
+    linux)
+      if is_glibc_compatible; then
+        platform="linux"
+      else
+        platform="linuxstatic"
+      fi
+      ;;
+    darwin) platform="macos" ;;
+    windows) platform="win" ;;
+    mingw*) platform="win" ;;
+  esac
+
+  printf '%s' "${platform}"
+}
+
+detect_arch() {
+  local arch
+  arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
+
+  case "${arch}" in
+    x86_64 | amd64) arch="x64" ;;
+    armv*) arch="arm" ;;
+    arm64 | aarch64) arch="arm64" ;;
+  esac
+
+  # `uname -m` in some cases mis-reports 32-bit OS as 64-bit, so double check
+  if [ "${arch}" = "x64" ] && [ "$(getconf LONG_BIT)" -eq 32 ]; then
+    arch=i686
+  elif [ "${arch}" = "arm64" ] && [ "$(getconf LONG_BIT)" -eq 32 ]; then
+    arch=arm
+  fi
+
+  case "$arch" in
+    x64*) ;;
+    arm64*) ;;
+    *) return 1
+  esac
+  printf '%s' "${arch}"
+}
+
+download_and_install() {
+  local platform arch version_json version archive_url tmp_dir
+  platform="$(detect_platform)"
+  arch="$(detect_arch)" || abort "Sorry! pnpm currently only provides pre-built binaries for x86_64/arm64 architectures."
+  if [ -z "${PNPM_VERSION}" ]; then
+    version_json="$(download "https://registry.npmjs.org/@pnpm/exe")" || abort "Download Error!"
+    version="$(echo "$version_json" | grep -o '"latest":[[:space:]]*"[0-9.]*"' | grep -o '[0-9.]*')"
+  else
+    version="${PNPM_VERSION}"
+  fi
+
+  archive_url="https://github.com/pnpm/pnpm/releases/download/v${version}/pnpm-${platform}-${arch}"
+  if [ "${platform}" = "win" ]; then
+    archive_url="${archive_url}.exe"
+  fi
+
+  # install to PNPM_HOME, defaulting to ~/.pnpm
+  tmp_dir="$(mktemp -d)" || abort "Tmpdir Error!"
+  trap 'rm -rf "$tmp_dir"' EXIT INT TERM HUP
+
+  ohai "Downloading pnpm binaries ${version}"
+  # download the binary to the specified directory
+  download "$archive_url" > "$tmp_dir/pnpm"  || return 1
+  chmod +x "$tmp_dir/pnpm"
+  copy_cli() {
+    local current_location="$1"
+    local target_dir="$2"
+    local new_exec_path="${target_dir}/$(basename "${current_location}")"
+
+    if [ "$(realpath "${new_exec_path}")" = "$(realpath "${current_location}")" ]; then
+      return
+    fi
+
+    echo "Copying pnpm CLI from ${current_location} to ${new_exec_path}"
+    mkdir -p "${target_dir}"
+    cp "${current_location}" "${new_exec_path}"
+  }
+
+  copy_cli "$tmp_dir/pnpm" "${PNPM_HOME:-$HOME/.kitpnpm}" || return 1
+}
+
+download_and_install || abort "Install Error!"
