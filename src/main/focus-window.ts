75c519fa625bf736ed3cd3f8119f68f532ea4c8d
johnlindquist <johnlindquist@gmail.com>
2025-01-31 22:39:10 -0700

    fix(window): focus window

--- src/main/focus-window.ts
+++ src/main/focus-window.ts
@@ -3,79 +3,64 @@
 // Keyword: w
 // Cache: true
 
-import "@johnlindquist/kit"
-import { escapeShortcut } from "../core/utils.js"
-import type { Choice } from "../types/core.js"
+import '@johnlindquist/kit'
+import { escapeShortcut } from '../core/utils.js'
+import type { Choice } from '../types/core.js'
 
-let isMac = process.platform === "darwin"
+let isMac = process.platform === 'darwin'
 
 if (isMac) {
-	type MacWindows = {
-		getWindows: (options: {
-			showAllWindows: boolean
-			onScreenOnly: boolean
-		}) => Promise<
-			{
-				name: string
-				ownerName: string
-				number: number
-				pid: number
-			}[]
-		>
-	}
-	let importKey = isMac ? "mac-windows" : ""
-	let { getWindows } = (await npm(importKey)) as MacWindows
+  let { getWindowsList } = await import('@johnlindquist/mac-windows')
 
-	let apps = await db<{ choices: Choice[] }>(kitPath("db", "apps.json"))
+  let apps = await db<{ choices: Choice[] }>(kitPath('db', 'apps.json'))
 
-	let windows = await getWindows({
-		showAllWindows: true,
-		onScreenOnly: false
-	})
+  let windows = await getWindowsList({
+    showAllWindows: true,
+    onScreenOnly: false
+  })
 
-	let ignore = [
-		"Notification Center",
-		"Dock",
-		"AvatarPickerMemojiPicker",
-		"com.apple.preference.security.r",
-		"Kit"
-	]
+  // Added extra junk windows to the ignore list and removed the name check
+  let ignore = [
+    'Notification Center',
+    'Dock',
+    'AvatarPickerMemojiPicker',
+    'com.apple.preference.security.r',
+    'Kit',
+    'CursorUIViewService',
+    'loginWindow',
+    'com.apple.PressAndHold',
+    'Script Kit',
+    'Spotlight'
+  ]
 
-	let selectedWindow = await arg<{
-		name: string
-		ownerName: string
-		number: number
-		pid: number
-	}>(
-		{
-			placeholder: "Focus Window",
-			enter: "Focus",
-			shortcuts: [escapeShortcut],
-			resize: true,
-			searchKeys: [
-				"slicedName",
-				"friendlyShortcut",
-				"tag",
-				"group",
-				"command",
-				"description"
-			]
-		},
-		windows
-			.filter((w) => !ignore.includes(w.ownerName) && w.name !== "")
-			.map((w) => {
-				let img =
-					(apps?.choices?.length ? apps.choices : []).find(
-						(a) => a.name === w.ownerName || a.name.includes(w.ownerName)
-					)?.img || ""
-				return {
-					name: w.ownerName,
-					description: w.name,
-					img,
-					value: w
-				}
-			})
-	)
-	await hide()
-	await focusWindow(selectedWindow.ownerName, selectedWindow.name)
+  let selectedWindow = await arg<{
+    name: string
+    ownerName: string
+    number: number
+    pid: number
+  }>(
+    {
+      placeholder: 'Focus Window',
+      enter: 'Focus',
+      shortcuts: [escapeShortcut],
+      resize: true,
+      searchKeys: ['slicedName', 'friendlyShortcut', 'tag', 'group', 'command', 'description']
+    },
+    windows
+      .filter((w) => !ignore.includes(w.ownerName))
+      .map((w) => {
+        let img =
+          (apps?.choices?.length ? apps.choices : []).find(
+            (a) => a.name === w.ownerName || a.name.includes(w.ownerName)
+          )?.img || ''
+        return {
+          name: w.ownerName,
+          description: w.name || w.ownerName, // fallback to ownerName when name is empty
+          img,
+          value: w
+        }
+      })
+  )
+  await hide()
+  await focusWindow(selectedWindow.ownerName, selectedWindow.name)
 }
